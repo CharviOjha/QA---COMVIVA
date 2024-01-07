fd2ce6c73b53566bc2f3d32cda14b764affad8d3
CharviOjha <155511644+CharviOjha@users.noreply.github.com>
2024-01-07 17:44:10 +0530

    Add files via upload

---
+++ JUnit Assignment/src/main/java/com/junit/EmailValidator.java
@@ -0,0 +1,12 @@
+package com.junit;
+
+public class EmailValidator {
+	public boolean isValidEmail(String email) {
+		 
+		 return email != null && email.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}");
+		 }
+		 public boolean isCorporateEmail(String email) {
+		 
+		 return email != null && email.endsWith("company.com");
+		 }
+}
